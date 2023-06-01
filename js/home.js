ee02bc528527585df8133913104f88aaeed8e478
“[MR <mizanurrahmanhridu@gmail.com>
2023-06-01 10:02:20 +0600

    Search Feature Implemented

--- js/home.js
+++ js/home.js
@@ -1,14 +1,17 @@
 // Retrieve tasks from local storage
 const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-function populateTable() {
+// Function to Display Task Table
+function populateTable(filteredTasks) {
     const tableBody = document.getElementById('taskTableBody');
     tableBody.innerHTML = '';
 
     // Retrieve authenticated user ID from local storage
     const authenticatedUserId = localStorage.getItem('authenticatedUserId');
 
-    tasks.forEach(task => {
+    const tasksToDisplay = filteredTasks || tasks; // Use filteredTasks if available, otherwise use all tasks
+
+    tasksToDisplay.forEach(task => {
         // Check if the task belongs to the authenticated user
         if (task.authenticatedUserId === authenticatedUserId) {
             const row = document.createElement('tr');
@@ -38,6 +41,7 @@ function populateTable() {
 }
 
 
+// Function to edit task
 function editTask(taskId) {
     // Find the task with the matching ID
     const task = tasks.find(t => t.id === taskId);
@@ -50,34 +54,184 @@ function editTask(taskId) {
 }
 
 
-// Retrieve the task ID and task data from the URL parameters
-const urlParams = new URLSearchParams(window.location.search);
-const taskId = urlParams.get('taskId');
-const taskNameParam = urlParams.get('taskName');
-const taskCategoryParam = urlParams.get('taskCategory');
-const taskStatusParam = urlParams.get('taskStatus');
-const taskDescriptionParam = urlParams.get('taskDescription');
-const responsiblePersonParam = urlParams.get('responsiblePerson');
-const startDateParam = urlParams.get('startDate');
-const endDateParam = urlParams.get('endDate');
-
-
 // Function to handle the delete button functionality
 function deleteTask(taskId) {
     // Find the task index with the matching ID
     const taskIndex = tasks.findIndex(t => t.id === taskId);
 
+
+
     if (taskIndex !== -1) {
-        // Remove the task from the array
-        tasks.splice(taskIndex, 1);
+        // Show a confirmation dialog
+        const confirmation = confirm(`Are you sure you want to delete the task, id : ${taskId} ?`);
 
-        // Update tasks in local storage
-        localStorage.setItem('tasks', JSON.stringify(tasks));
+        if (confirmation) {
+            // Remove the task from the array
+            tasks.splice(taskIndex, 1);
 
-        // Repopulate the table
-        populateTable();
+            // Update tasks in local storage
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+
+            // Repopulate the table
+            populateTable();
+        }
     }
 }
 
+
 // Initial population of the table
 populateTable();
+
+// Search
+// function showList() {
+//     // Retrieve data from local storage
+//     const tasksData = localStorage.getItem('tasks');
+//     const tasks = JSON.parse(tasksData) || [];
+//
+//     const filteredTasks = tasks.filter(task => {
+//         const {
+//             taskName,
+//             taskCategory,
+//             taskStatus,
+//             responsiblePerson,
+//             endDate
+//         } = searchItem;
+//
+//         if (taskName && !task.taskName.toLowerCase().includes(taskName)) {
+//             return false;
+//         }
+//
+//         if (taskCategory.length > 0 && !taskCategory.includes(task.taskCategory)) {
+//             return false;
+//         }
+//
+//         if (taskStatus && task.taskStatus !== taskStatus) {
+//             return false;
+//         }
+//
+//         if (responsiblePerson && !task.responsiblePerson.toLowerCase().includes(responsiblePerson.toLowerCase())) {
+//             return false;
+//         }
+//
+//         if (endDate && task.endDate !== endDate) {
+//             return false;
+//         }
+//
+//         return true;
+//     });
+//
+//
+//     // Display the filtered tasks
+//     populateTable(filteredTasks);
+// }
+function showList() {
+    // Retrieve data from local storage
+    const tasksData = localStorage.getItem('tasks');
+    const tasks = JSON.parse(tasksData) || [];
+
+    const filteredTasks = tasks.filter(task => {
+        const {
+            taskName,
+            taskCategory,
+            taskStatus,
+            responsiblePerson,
+            endDate
+        } = searchItem;
+
+        if (taskName && !task.taskName.toLowerCase().includes(taskName)) {
+            return false;
+        }
+
+        if (taskCategory.length > 0 && !taskCategory.includes(task.taskCategory)) {
+            return false;
+        }
+
+        if (taskStatus && task.taskStatus !== taskStatus) {
+            return false;
+        }
+
+        if (responsiblePerson && !task.responsiblePerson.toLowerCase().includes(responsiblePerson.toLowerCase())) {
+            return false;
+        }
+
+        if (endDate && task.endDate !== endDate) {
+            return false;
+        }
+
+        return true;
+    });
+
+    // Display the filtered tasks
+    populateTable(filteredTasks);
+}
+
+
+
+var searchItem = {};
+
+// function handleSearch(event) {
+//     event.preventDefault(); // Prevent form submission
+//
+//     // Retrieve search criteria from form inputs
+//     const taskNameSearch = document.getElementById('taskNameSearch').value;
+//     // const taskCategorySearch = Array.from(document.querySelectorAll('input[name="taskCategorySearch"]:checked')).map(checkbox => checkbox.value);
+//     const taskCategoryInputs = Array.from(document.querySelectorAll('input[name="taskCategorySearch"]:checked'));
+//     const taskCategorySearch = taskCategoryInputs.map(checkbox => checkbox.value);
+//
+//     const taskStatusInput = document.querySelector('input[name="taskStatus"]:checked');
+//     const taskStatusSearch = taskStatusInput ? taskStatusInput.value : '';
+//     const responsiblePersonSearch = document.getElementById('responsiblePersonSearch').value;
+//     const endDateSearch = document.getElementById('endDateSearch').value;
+//     debugger;
+//     console.log(taskCategorySearch);
+//     let tName = taskNameSearch.trim()
+//     tName = tName.toLowerCase();
+//     searchItem = {
+//         taskName: tName,
+//         taskCategory: taskCategorySearch,
+//         taskStatus: taskStatusSearch,
+//         responsiblePerson: responsiblePersonSearch,
+//         endDate: endDateSearch,
+//     };
+//     // debugger;
+//     // console.log(searchItem);
+//     showList();
+//
+// }
+function handleSearch(event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Retrieve search criteria from form inputs
+    const taskNameSearch = document.getElementById('taskNameSearch').value;
+
+    const taskCategoryInputs = Array.from(document.querySelectorAll('input[name="taskCategory"]:checked'));
+    const taskCategorySearch = taskCategoryInputs.map(checkbox => checkbox.value);
+
+    const taskStatusInput = document.querySelector('input[name="taskStatus"]:checked');
+    const taskStatusSearch = taskStatusInput ? taskStatusInput.value : '';
+
+    const responsiblePersonSearch = document.getElementById('responsiblePersonSearch').value;
+    const endDateSearch = document.getElementById('endDateSearch').value;
+    debugger;
+    console.log(taskCategorySearch);
+
+    let tName = taskNameSearch.trim();
+    tName = tName.toLowerCase();
+
+    searchItem = {
+        taskName: tName,
+        taskCategory: taskCategorySearch,
+        taskStatus: taskStatusSearch,
+        responsiblePerson: responsiblePersonSearch,
+        endDate: endDateSearch,
+    };
+    // debugger;
+    console.log(searchItem);
+
+    showList();
+}
+
+// debugger;
+// console.log("hridu");
+// Add event listener to the search form
+document.getElementById('searchForm').addEventListener('submit', handleSearch);
